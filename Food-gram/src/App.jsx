34000eff50ef472bff202039edb20bb36806d2b9
rafidmahin20 <submanmahin@gmail.com>
2024-01-06 20:07:16 +0600

    change the logo

--- Food-gram/src/App.jsx
+++ Food-gram/src/App.jsx
@@ -33,7 +33,7 @@ const App = () => {
     <div className="flex">
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-4">Food Menu</h1>
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 gap-2 sm:grid-cols-1 lg:grid-cols-2">
         {initialMenu.map((item) => (
           <div key={item.id} className="mb-4">
             <FoodItem
